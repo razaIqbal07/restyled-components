ea6142229fb2199d176533c2b57e059f0ef76f18
Raza Iqbal <mailrazaiqbal@gmail.com>
2021-03-24 11:19:20 +0530

    styled link

--- src/components/StyledLink.js
+++ src/components/StyledLink.js
@@ -5,9 +5,17 @@ const StyledLink = styled.a(
    font-size : ${props.fontSize ? props.fontSize : '12px'};
    font-weight : ${props.fontWeight ? props.fontWeight : '500'};
    letter-spacing  :  ${props.fontSpacing ? props.fontSpacing : '1px'};
-   color : ${props.color ? props.color : '#333'};
+   color : ${props.color ? props.color : '#000000'};
    margin : ${props.margin ? props.margin : 'none'};
+   margin-top : ${props.marginTop ? props.marginTop : 'none'};
+   margin-bottom : ${props.marginBottom ? props.marginBottom : 'none'};
+   margin-left : ${props.marginLeft ? props.marginLeft : 'none'};
+   margin-right : ${props.marginRight ? props.marginRight : 'none'};
    padding : ${props.padding ? props.padding : 'none'};
+   padding-top : ${props.paddingTop ? props.paddingTop : 'none'};
+   padding-bottom : ${props.paddingBottom ? props.paddingBottom : 'none'};
+   padding-left : ${props.paddingLeft ? props.paddingLeft : 'none'};
+   padding-right : ${props.paddingRight ? props.paddingRight : 'none'};
    user-select : ${props.userSelect ? props.userSelect : 'auto'};
    text-decoration :  ${props.textDecoration ? props.textDecoration : 'none'};
   `
